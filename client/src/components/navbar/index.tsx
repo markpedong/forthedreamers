0db7404ef4a962cd6499397d05cd910a96026150
atlas <atlas.wbridges@gmail.com>
2024-07-04 17:27:26 +0800

    - added more page.

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -51,7 +51,7 @@ const Navbar: FC = () => {
 				<div className={classNames(styles.leftBtnWrapper, roboto.className)}>
 					<span onClick={() => push('/')}>HOME</span>
 					<span onClick={() => push('/shop')}>SHOP</span>
-					<span onClick={() => push('/collections')}>COLLECTIONS</span>
+					<span onClick={() => push('/collection')}>COLLECTIONS</span>
 					<span onClick={() => push('/support')}>SUPPORT</span>
 				</div>
 			)}
