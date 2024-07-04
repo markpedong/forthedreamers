7c5148ac51bd82cb6d341c0d6907ff711361b7d2
markpedong <mark.pedong01@gmail.com>
2024-07-04 20:46:59 +0800

    chore: finished collections page.

--- client/src/app/collection/page.tsx
+++ client/src/app/collection/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import styles from './styles.module.scss'
+import Category from './components/category'
+import classNames from 'classnames'
+import { FaArrowRight } from 'react-icons/fa'
+import { Roboto_Condensed } from 'next/font/google'
 
-type Props = {}
+const roboto = Roboto_Condensed({ weight: '300', subsets: ['latin'] })
 
-const Page = (props: Props) => {
-  return (
-    <div>Page</div>
-  )
+const Page = () => {
+	return (
+		<div className={styles.mainWrapper}>
+			<div className={styles.pageTitle}>SHOP BY CATEGORY</div>
+			<div className={styles.categoryWrapper}>
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+				<Category />
+			</div>
+			<div className={classNames(styles.pagination, roboto.className)}>
+				<span>1</span>
+				<span>2</span>
+				<span>3</span>
+				<span>4</span>
+				<span>
+					<FaArrowRight />
+				</span>
+			</div>
+		</div>
+	)
 }
 
 export default Page
