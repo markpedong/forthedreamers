f96d80e02bdec9d101a141d8322a0364a14de3df
atlas <atlas.wbridges@gmail.com>
2024-07-05 13:13:56 +0800

    chore: footer animation.

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -17,7 +17,8 @@ const Footer = () => {
 	const [open, setOpen] = useState(false)
 	const [showLanguage, setShowLanguage] = useState(false)
 	const [showCurrency, setShowCurrency] = useState(false)
-	const small = useMediaQuery('only screen and (min-width : 993px)')
+	const small = useMediaQuery('only screen and (max-width : 576px)')
+	const medium = useMediaQuery('only screen and (min-width : 993px)')
 
 	return (
 		<div className={classNames(styles.footerContainer, roboto.className)}>
@@ -25,10 +26,10 @@ const Footer = () => {
 				<div>
 					<h2>
 						SUPPORT
-						{!small && open ? <FaMinus className={styles.minusIcon} onClick={() => setOpen(false)} /> : <FaPlus className={styles.plusIcon} onClick={() => setOpen(true)} />}
+						{!medium && open ? <FaMinus className={styles.minusIcon} onClick={() => setOpen(false)} /> : <FaPlus className={styles.plusIcon} onClick={() => setOpen(true)} />}
 					</h2>
 					<AnimatePresence>
-						{(open || small) && (
+						{(open || medium) && (
 							<motion.div
 								className={styles.menuContainer}
 								initial={{ opacity: 0, height: 0, y: -10 }}
@@ -71,10 +72,13 @@ const Footer = () => {
 							{showLanguage && (
 								<motion.div
 									className={styles.options}
-									initial={{ opacity: 0, height: 0, y: -10 }}
-									animate={{ opacity: 1, height: 'auto', y: 0 }}
-									exit={{ opacity: 0, height: 0, y: -10 }}
-									transition={{ duration: 0.3 }}
+									initial={small ? { opacity: 0, top: '-9rem' } : { opacity: 0, height: 0, y: -10 }}
+									exit={small ? { opacity: 0, top: '-9rem' } : { opacity: 0, height: 0, y: -10 }}
+									animate={
+										small
+											? { opacity: 1, top: '-8.3rem', animation: 'ease-out', transition: { duration: 0.3 } }
+											: { opacity: 1, height: 'auto', y: 0, transition: { duration: 0.3 } }
+									}
 								>
 									<span>Japanese</span>
 									<span>Japanese</span>
@@ -98,16 +102,19 @@ const Footer = () => {
 							{showCurrency && (
 								<motion.div
 									className={styles.options}
-									initial={{ opacity: 0, height: 0, y: -10 }}
-									animate={{ opacity: 1, height: 'auto', y: 0 }}
-									exit={{ opacity: 0, height: 0, y: -10 }}
-									transition={{ duration: 0.3 }}
+									initial={small ? { opacity: 0, top: '-9rem' } : { opacity: 0, height: 0, y: -10 }}
+									exit={small ? { opacity: 0, top: '-9rem' } : { opacity: 0, height: 0, y: -10 }}
+									animate={
+										small
+											? { opacity: 1, top: '-8.3rem', animation: 'ease-out', transition: { duration: 0.3 } }
+											: { opacity: 1, height: 'auto', y: 0, transition: { duration: 0.3 } }
+									}
 								>
-									<span>Philippines (PHP ₱)</span>
-									<span>Philippines (PHP ₱)</span>
-									<span>Philippines (PHP ₱)</span>
-									<span>Philippines (PHP ₱)</span>
-									<span>Philippines (PHP ₱)</span>
+									<span>1 Philippines (PHP ₱)</span>
+									<span>2 Philippines (PHP ₱)</span>
+									<span>3 Philippines (PHP ₱)</span>
+									<span>4 Philippines (PHP ₱)</span>
+									<span>5 Philippines (PHP ₱)</span>
 								</motion.div>
 							)}
 						</AnimatePresence>
