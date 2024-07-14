a080afc42fc04c014567f4f8e5302d6b3705f7db
markpedong <mark.pedong01@gmail.com>
2024-07-14 22:30:52 +0800

    marquee

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -14,6 +14,7 @@ import MobileMenu from './components/mobile-menu'
 import { FiUser } from 'react-icons/fi'
 import Search from './components/search'
 import Cart from './components/cart'
+import Marquee from 'react-fast-marquee'
 
 const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] })
 const roboto = Roboto_Condensed({ weight: '300', subsets: ['latin'] })
@@ -37,91 +38,105 @@ const Navbar: FC = () => {
 	}
 
 	return (
-		<motion.div
-			className={styles.navbarWrapper}
-			onHoverStart={() => setIsHovering(!search && true)}
-			onHoverEnd={() => setIsHovering(false)}
-			style={
-				pathname === '/'
-					? { color: isWhiteBG ? 'black' : 'white' }
-					: { borderBottom: '0.1rem solid rgba(0, 0, 0, 0.2)' }
-			}
-		>
+		<>
+			{pathname !== '/' && (
+				<Marquee
+					className={classNames(roboto.className, 'bg-black text-white py-2 font-normal tracking-wide')}
+					direction="right"
+				>
+					<span className="text-[0.82rem]">
+						Same Day Delivery is unavailable today due to our ongoing free shipping promo.
+					</span>
+				</Marquee>
+			)}
 			<motion.div
-				className={styles.background}
-				initial={{ y: pathname === '/' ? '-100%' : 0 }}
-				animate={pathname === '/' ? { y: isWhiteBG ? 0 : '-100%', transition: { duration: 0.15, ease: 'easeIn' } } : {}}
-			/>
-			{width! > 992 && (
-				<div className={classNames(styles.leftBtnWrapper, roboto.className)}>
-					<span onClick={() => handlePush('/')}>HOME</span>
-					<span onClick={() => handlePush('/shop')}>SHOP</span>
-					<span onClick={() => handlePush('/collection')}>COLLECTIONS</span>
-					<motion.div className={styles.supportContainer} onClick={() => push('/support')}>
-						<span
-							onMouseEnter={() => {
-								setShowDropdown(true)
-							}}
-						>
-							SUPPORT
-						</span>
-						<FaChevronDown />
-					</motion.div>
-				</div>
-			)}
-			{width! < 993 && (
-				<div className={styles.mobileBtnWrapper}>
-					{open && <IoClose onClick={() => setOpen(false)} size={30} />}
-					{!open && (
-						<IoMenu
-							onClick={() => {
-								setOpen(true)
-							}}
-							size={30}
-						/>
-					)}
-					<AnimatePresence>
-						{open && (
-							<motion.div
-								className={classNames(styles.drawerContainer, roboto.className)}
-								initial={{ opacity: 0, top: '150%%' }}
-								exit={{ opacity: 0, top: '150%%' }}
-								animate={{ opacity: 1, top: '5.6rem', animation: 'ease-out', transition: { duration: 0.5 } }}
+				className={styles.navbarWrapper}
+				onHoverStart={() => setIsHovering(!search && true)}
+				onHoverEnd={() => setIsHovering(false)}
+				style={
+					pathname === '/'
+						? { color: isWhiteBG ? 'black' : 'white' }
+						: { borderBottom: '0.1rem solid rgba(0, 0, 0, 0.2)', top: y! > 40 ? 0 : 40 }
+				}
+			>
+				<motion.div
+					className={styles.background}
+					initial={{ y: pathname === '/' ? '-100%' : 0 }}
+					animate={
+						pathname === '/' ? { y: isWhiteBG ? 0 : '-100%', transition: { duration: 0.15, ease: 'easeIn' } } : {}
+					}
+				/>
+				{width! > 992 && (
+					<div className={classNames(styles.leftBtnWrapper, roboto.className)}>
+						<span onClick={() => handlePush('/')}>HOME</span>
+						<span onClick={() => handlePush('/shop')}>SHOP</span>
+						<span onClick={() => handlePush('/collection')}>COLLECTIONS</span>
+						<motion.div className={styles.supportContainer} onClick={() => push('/support')}>
+							<span
+								onMouseEnter={() => {
+									setShowDropdown(true)
+								}}
 							>
-								<MobileMenu setOpen={() => setOpen(false)} />
-							</motion.div>
-						)}
-					</AnimatePresence>
-				</div>
-			)}
-			<div className={classNames(styles.navTitle, poppins.className)}>FOR THE DREAMERS</div>
-			<div className={classNames(styles.rightBtnWrapper, roboto.className)}>
-				<div className={styles.loginBtn}>
-					<span>LOGIN</span>
-					<FiUser size={25} />
-				</div>
-				<IoSearchOutline size={25} onClick={() => setSearch(true)} />
-				<CiShoppingCart size={25} onClick={() => setShowCart(true)} />
-			</div>
-			<AnimatePresence>{showCart && <Cart setShowCart={() => setShowCart(false)} />}</AnimatePresence>
-			<AnimatePresence>{search && <Search setSearch={() => setSearch(false)} />}</AnimatePresence>
-			<AnimatePresence>
-				{width! > 1068 && showDropdown && (
-					<motion.div
-						className={styles.dropdownMenu}
-						onMouseLeave={() => setShowDropdown(false)}
-						initial={{ opacity: 0 }}
-						exit={{ opacity: 0 }}
-						animate={{ opacity: 1, animation: 'ease-out', transition: { duration: 0.5 } }}
-					>
-						<span onClick={() => handlePush('/support/orders-payment')}>ORDERS & PAYMENT</span>
-						<span onClick={() => handlePush('/support/shipping')}>SHIPPING</span>
-						<span onClick={() => handlePush('/support/returns')}>RETURNS</span>
-						<span onClick={() => handlePush('/support/gift-card-manual')}>GIFT CARD MANUAL</span>
-					</motion.div>
+								SUPPORT
+							</span>
+							<FaChevronDown />
+						</motion.div>
+					</div>
 				)}
-			</AnimatePresence>
-		</motion.div>
+				{width! < 993 && (
+					<div className={styles.mobileBtnWrapper}>
+						{open && <IoClose onClick={() => setOpen(false)} size={30} />}
+						{!open && (
+							<IoMenu
+								onClick={() => {
+									setOpen(true)
+								}}
+								size={30}
+							/>
+						)}
+						<AnimatePresence>
+							{open && (
+								<motion.div
+									className={classNames(styles.drawerContainer, roboto.className)}
+									initial={{ opacity: 0, top: '150%%' }}
+									exit={{ opacity: 0, top: '150%%' }}
+									animate={{ opacity: 1, top: '5.6rem', animation: 'ease-out', transition: { duration: 0.5 } }}
+								>
+									<MobileMenu setOpen={() => setOpen(false)} />
+								</motion.div>
+							)}
+						</AnimatePresence>
+					</div>
+				)}
+				<div className={classNames(styles.navTitle, poppins.className)}>FOR THE DREAMERS</div>
+				<div className={classNames(styles.rightBtnWrapper, roboto.className)}>
+					<div className={styles.loginBtn}>
+						<span>LOGIN</span>
+						<FiUser size={25} />
+					</div>
+					<IoSearchOutline size={25} onClick={() => setSearch(true)} />
+					<CiShoppingCart size={25} onClick={() => setShowCart(true)} />
+				</div>
+				<AnimatePresence>{showCart && <Cart setShowCart={() => setShowCart(false)} />}</AnimatePresence>
+				<AnimatePresence>{search && <Search setSearch={() => setSearch(false)} />}</AnimatePresence>
+				<AnimatePresence>
+					{width! > 1068 && showDropdown && (
+						<motion.div
+							className={styles.dropdownMenu}
+							onMouseLeave={() => setShowDropdown(false)}
+							initial={{ opacity: 0 }}
+							exit={{ opacity: 0 }}
+							animate={{ opacity: 1, animation: 'ease-out', transition: { duration: 0.5 } }}
+						>
+							<span onClick={() => handlePush('/support/orders-payment')}>ORDERS & PAYMENT</span>
+							<span onClick={() => handlePush('/support/shipping')}>SHIPPING</span>
+							<span onClick={() => handlePush('/support/returns')}>RETURNS</span>
+							<span onClick={() => handlePush('/support/gift-card-manual')}>GIFT CARD MANUAL</span>
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</motion.div>
+		</>
 	)
 }
 
