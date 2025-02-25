e4dd4b019016cd504b19582f0456c3644c0f618d
markpedong <mark.pedong01@gmail.com>
2025-02-25 18:59:48 +0800

    no message

--- app/_components/navbar/index.tsx
+++ app/_components/navbar/index.tsx
@@ -1,79 +1,82 @@
 'use client'
 
+import { NO_NAVBAR_PAGES } from '@/app/_constants'
 import {
-	Button,
 	Link,
 	Navbar,
-	NavbarBrand,
 	NavbarContent,
 	NavbarItem,
 	NavbarMenu,
 	NavbarMenuItem,
 	NavbarMenuToggle
 } from '@heroui/react'
+import { usePathname } from 'next/navigation'
 import { FC, useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 const NavBar: FC = () => {
+	const pathname = usePathname()
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
 	const menuItems = ['Home', 'Shop', 'Collection', 'Support']
 
 	return (
-		<Navbar onMenuOpenChange={setIsMenuOpen}>
-			<NavbarContent>
-				<NavbarMenuToggle
-					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-					className="md:hidden"
-				/>
-				<NavbarContent className="hidden md:flex gap-4" justify="center">
-					<NavbarItem>
-						<Link color="foreground" href="#">
-							Home
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link aria-current="page" href="#">
-							Shop
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link color="foreground" href="#">
-							Collection
-						</Link>
-					</NavbarItem>
-					<NavbarItem>
-						<Link color="foreground" href="#">
-							Support
-						</Link>
+		!NO_NAVBAR_PAGES.includes(pathname) && (
+			<Navbar onMenuOpenChange={setIsMenuOpen} isBordered>
+				<NavbarContent>
+					<NavbarMenuToggle
+						aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+						className="md:hidden"
+					/>
+					<NavbarContent className="hidden md:flex gap-4" justify="center">
+						<NavbarItem className="bg-red-500">
+							<Link color="foreground" href="#">
+								Home
+							</Link>
+						</NavbarItem>
+						<NavbarItem>
+							<Link aria-current="page" href="#">
+								Shop
+							</Link>
+						</NavbarItem>
+						<NavbarItem>
+							<Link color="foreground" href="#">
+								Collection
+							</Link>
+						</NavbarItem>
+						<NavbarItem>
+							<Link color="foreground" href="#">
+								Support
+							</Link>
+						</NavbarItem>
+					</NavbarContent>
+				</NavbarContent>
+				<NavbarContent justify="center">
+					<p className="font-bold text-inherit">For the Dreamers</p>
+				</NavbarContent>
+				<NavbarContent justify="end">
+					<FaSearch />
+					<NavbarItem className="hidden md:flex">
+						<Link href="/login">Login</Link>
 					</NavbarItem>
 				</NavbarContent>
-			</NavbarContent>
-			<NavbarContent justify="center">
-				<p className="font-bold text-inherit">For the Dreamers</p>
-			</NavbarContent>
-			<NavbarContent justify="end">
-				<FaSearch />
-				<NavbarItem className="hidden md:flex">
-					<Link href="#">Login</Link>
-				</NavbarItem>
-			</NavbarContent>
-			<NavbarMenu>
-				{menuItems.map((item, index) => (
-					<NavbarMenuItem key={`${item}-${index}`}>
-						<Link
-							className="w-full"
-							color={
-								index === 2 ? 'primary' : index === menuItems.length - 1 ? 'danger' : 'foreground'
-							}
-							href="#"
-							size="lg"
-						>
-							{item}
-						</Link>
-					</NavbarMenuItem>
-				))}
-			</NavbarMenu>
-		</Navbar>
+				<NavbarMenu>
+					{menuItems.map((item, index) => (
+						<NavbarMenuItem key={`${item}-${index}`}>
+							<Link
+								className="w-full"
+								color={
+									index === 2 ? 'primary' : index === menuItems.length - 1 ? 'danger' : 'foreground'
+								}
+								href="#"
+								size="lg"
+							>
+								{item}
+							</Link>
+						</NavbarMenuItem>
+					))}
+				</NavbarMenu>
+			</Navbar>
+		)
 	)
 }
 
