2420cfa3dde7bf8072d08e48c6d8f093606a33bf
markpedong <mark.pedong01@gmail.com>
2025-02-25 17:27:33 +0800

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const nextConfig = {
-  output: "standalone",
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    additionalData: `@import "./src/styles/_helpers.scss";`,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
