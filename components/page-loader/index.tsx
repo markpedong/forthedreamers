08e783df9476733b9b9758a99c8301ba356bc466
markpedong <mark.pedong01@gmail.com>
2025-02-26 09:57:23 +0800

    fixed: loader

--- components/page-loader/index.tsx
+++ components/page-loader/index.tsx
@@ -3,12 +3,11 @@ import { Spinner } from '@heroui/react'
 import { FC } from 'react'
 
 const PageLoader: FC = () => {
-  return (
-    <div className="absolute w-full h-full flex flex-col items-center justify-center">
-      <Spinner />
-      <span className="mt-2 dark:text-white">Loading...</span>
-    </div>
-  )
+	return (
+		<div className="absolute w-full h-full flex flex-col items-center justify-center">
+			<Spinner label="Loading..." variant="default" />
+		</div>
+	)
 }
 
 export default PageLoader
