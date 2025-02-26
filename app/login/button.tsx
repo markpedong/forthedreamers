d10d645196011b13e9096a24b559d11af67e951b
markpedong <mark.pedong01@gmail.com>
2025-02-26 22:43:23 +0800

    dark mode minor changes.

--- app/login/button.tsx
+++ app/login/button.tsx
@@ -4,50 +4,56 @@ import { LOGINFORM_STATE } from '@/constants/types'
 import { signIn } from 'next-auth/react'
 import { useAppSelector } from '@/redux/store'
 import { FC } from 'react'
+import { useTheme } from 'next-themes'
+import classNames from 'classnames'
 
 interface AuthButtonsProps {
-  isPending: boolean
+	isPending: boolean
 }
 
 const AuthButtons: FC<AuthButtonsProps> = ({ isPending }) => {
-  const loginFormState = useAppSelector(state => state.app.loginFormState)
+	const loginFormState = useAppSelector(state => state.app.loginFormState)
+	const { theme } = useTheme()
 
-  return (
-    <>
-      <Button
-        type="submit"
-        isLoading={isPending}
-        fullWidth
-        className="mt-5 bg-black text-white"
-        variant="shadow"
-        radius="sm"
-      >
-        {loginFormState === LOGINFORM_STATE.FORGOT_PASSWORD
-          ? isPending
-            ? 'Submitting...'
-            : 'Submit'
-          : loginFormState === LOGINFORM_STATE.REGISTER
-          ? isPending
-            ? 'Registering...'
-            : 'Sign up'
-          : isPending
-          ? 'Signing in...'
-          : 'Sign in'}
-      </Button>
-      {loginFormState !== LOGINFORM_STATE.FORGOT_PASSWORD && (
-        <Button
-          color="default"
-          startContent={<FcGoogle />}
-          variant="bordered"
-          fullWidth
-          className="mt-2"
-          onPress={async () => await signIn('google', { callbackUrl: '/profile', redirect: true })}
-        >
-          {loginFormState === LOGINFORM_STATE.REGISTER ? 'Sign up with Google' : 'Sign in with Google'}
-        </Button>
-      )}
-    </>
-  )
+	return (
+		<>
+			<Button
+				type="submit"
+				isLoading={isPending}
+				fullWidth
+				className={classNames('mt-5', {
+					'bg-white text-black': theme === 'dark',
+					'bg-black text-white': theme === 'light'
+				})}
+				variant="shadow"
+				radius="sm"
+			>
+				{loginFormState === LOGINFORM_STATE.FORGOT_PASSWORD
+					? isPending
+						? 'Submitting...'
+						: 'Submit'
+					: loginFormState === LOGINFORM_STATE.REGISTER
+					? isPending
+						? 'Registering...'
+						: 'Sign up'
+					: isPending
+					? 'Signing in...'
+					: 'Sign in'}
+			</Button>
+			{loginFormState !== LOGINFORM_STATE.FORGOT_PASSWORD && (
+				<Button
+					color="default"
+					startContent={<FcGoogle />}
+					variant="bordered"
+					fullWidth
+					className="mt-2"
+					onPress={async () => await signIn('google', { callbackUrl: '/profile', redirect: true })}
+				>
+					{loginFormState === LOGINFORM_STATE.REGISTER ? 'Sign up with Google' : 'Sign in with Google'}
+				</Button>
+			)}
+		</>
+	)
 }
 
 export default AuthButtons
