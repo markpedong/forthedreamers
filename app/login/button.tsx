9afcb4f009f5a49457d9026564ff0f1dded14b3c
atlas <atlas.wbridges@gmail.com>
2025-02-27 22:08:57 +0800

    no message

--- app/login/button.tsx
+++ app/login/button.tsx
@@ -1,59 +1,65 @@
 import { Button } from '@heroui/react'
 import { FcGoogle } from 'react-icons/fc'
 import { LOGINFORM_STATE } from '@/constants/types'
-import { signIn } from 'next-auth/react'
+import { getSession, signIn } from 'next-auth/react'
 import { useAppSelector } from '@/redux/store'
 import { FC } from 'react'
 import { useTheme } from 'next-themes'
 import classNames from 'classnames'
 
 interface AuthButtonsProps {
-	isPending: boolean
+  isPending: boolean
 }
 
 const AuthButtons: FC<AuthButtonsProps> = ({ isPending }) => {
-	const loginFormState = useAppSelector(state => state.app.loginFormState)
-	const { theme } = useTheme()
+  const loginFormState = useAppSelector(state => state.app.loginFormState)
+  const { theme } = useTheme()
 
-	return (
-		<>
-			<Button
-				type="submit"
-				isLoading={isPending}
-				fullWidth
-				className={classNames('mt-5', {
-					'bg-white text-black': theme === 'dark',
-					'bg-black text-white': theme === 'light'
-				})}
-				variant="shadow"
-				radius="sm"
-			>
-				{loginFormState === LOGINFORM_STATE.FORGOT_PASSWORD
-					? isPending
-						? 'Submitting...'
-						: 'Submit'
-					: loginFormState === LOGINFORM_STATE.REGISTER
-					? isPending
-						? 'Registering...'
-						: 'Sign up'
-					: isPending
-					? 'Signing in...'
-					: 'Sign in'}
-			</Button>
-			{loginFormState !== LOGINFORM_STATE.FORGOT_PASSWORD && (
-				<Button
-					color="default"
-					startContent={<FcGoogle />}
-					variant="bordered"
-					fullWidth
-					className="mt-2"
-					onPress={async () => await signIn('google', { callbackUrl: '/profile', redirect: true })}
-				>
-					{loginFormState === LOGINFORM_STATE.REGISTER ? 'Sign up with Google' : 'Sign in with Google'}
-				</Button>
-			)}
-		</>
-	)
+  return (
+    <>
+      <Button
+        type="submit"
+        isLoading={isPending}
+        fullWidth
+        className={classNames('mt-5', {
+          'bg-white text-black': theme === 'dark',
+          'bg-black text-white': theme === 'light'
+        })}
+        variant="shadow"
+        radius="sm"
+      >
+        {loginFormState === LOGINFORM_STATE.FORGOT_PASSWORD
+          ? isPending
+            ? 'Submitting...'
+            : 'Submit'
+          : loginFormState === LOGINFORM_STATE.REGISTER
+          ? isPending
+            ? 'Registering...'
+            : 'Sign up'
+          : isPending
+          ? 'Signing in...'
+          : 'Sign in'}
+      </Button>
+      {loginFormState !== LOGINFORM_STATE.FORGOT_PASSWORD && (
+        <Button
+          color="default"
+          startContent={<FcGoogle />}
+          variant="bordered"
+          fullWidth
+          className="mt-2"
+          onPress={async () => {
+            const callback = await signIn('google', { callbackUrl: '/profile', redirect: false })
+            if (callback?.ok) {
+              const session = await getSession()
+              localStorage.setItem('accessToken', session?.accessToken || '')
+            }
+          }}
+        >
+          {loginFormState === LOGINFORM_STATE.REGISTER ? 'Sign up with Google' : 'Sign in with Google'}
+        </Button>
+      )}
+    </>
+  )
 }
 
 export default AuthButtons
