22aba680881e6e6838738b38db7b6d29fe4c7cab
markpedong <mark.pedong01@gmail.com>
2025-02-27 23:28:07 +0800

    de

--- app/profile/components/index.tsx
+++ app/profile/components/index.tsx
@@ -1,20 +1,12 @@
 'use client'
 
 import { Button, Divider } from '@heroui/react'
-import { signOut, useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
 import styles from '../styles.module.scss'
-import { useEffect } from 'react'
-import { getUserData } from '@/utils/request'
 
 type Props = {}
 
 const Profile = (props: Props) => {
-	const { data: session } = useSession()
-	useEffect(() => {
-    console.log('session', session)
-		getUserData(session?.user?.id as string)
-	}, [])
-
 	return (
 		<div className={styles.profileWrapper}>
 			<div className={styles.profileContainer}>
