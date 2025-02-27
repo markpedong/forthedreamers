a7af89010cbc147fd4340796ddcb8d9e451f7015
markpedong <mark.pedong01@gmail.com>
2025-02-27 23:27:42 +0800

    removed code

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,9 @@
-import { authOptions } from '../api/auth/[...nextauth]/route'
 import Profile from './components'
-import { getServerSession } from 'next-auth'
 
 type Props = {}
 
 const Page = async (props: Props) => {
-  const session = await getServerSession(authOptions)
-  // const res = await getUserData(session?.user?.id as string)
-
-  // console.log('res', res)
-  return <Profile />
+	return <Profile />
 }
 
 export default Page
