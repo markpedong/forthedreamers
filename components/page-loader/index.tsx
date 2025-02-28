6097af2df9ab0a2f4d6c5110decd946b1c820203
markpedong <mark.pedong01@gmail.com>
2025-03-01 00:56:28 +0800

    fix loader.

--- components/page-loader/index.tsx
+++ components/page-loader/index.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react'
 const PageLoader: FC = () => {
 	return (
 		<div className="absolute w-full h-full flex flex-col items-center justify-center">
-			<Spinner label="Loading..." variant="default" />
+			<Spinner label="Loading..." variant="spinner" />
 		</div>
 	)
 }
