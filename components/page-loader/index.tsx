1929308c82c1815b34ed75abdf4094aacb630a09
atlas <atlas.wbridges@gmail.com>
2025-03-01 10:34:54 +0800

    no message

--- components/page-loader/index.tsx
+++ components/page-loader/index.tsx
@@ -3,11 +3,11 @@ import { Spinner } from '@heroui/react'
 import { FC } from 'react'
 
 const PageLoader: FC = () => {
-	return (
-		<div className="absolute w-full h-full flex flex-col items-center justify-center">
-			<Spinner label="Loading..." variant="default" />
-		</div>
-	)
+  return (
+    <div className="absolute w-full h-full flex flex-col items-center justify-center">
+      <Spinner label="Loading..." variant="default" color="default" labelColor="foreground" size="lg" />
+    </div>
+  )
 }
 
 export default PageLoader
