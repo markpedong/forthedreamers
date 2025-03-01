944b44fddb52943867136f940ead96de37feee4c
atlas <atlas.wbridges@gmail.com>
2025-03-01 10:04:05 +0800

    Revert "fix loader."
    This reverts commit 6097af2df9ab0a2f4d6c5110decd946b1c820203.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import { SF_PRO_DISPLAY } from '@/public/fonts'
-import '../globals.scss'
+import './globals.scss'
 import Provider from '@/components/provider'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode
+}>) {
 	return (
 		<html lang="en">
 			<body className={SF_PRO_DISPLAY.className}>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,7 @@
 import { getUserData } from '@/utils/request'
 import Profile from './components'
 import { getServerSession } from 'next-auth'
-import authOptions from '@/app/api/auth/[...nextauth]/options'
+import authOptions from '../api/auth/[...nextauth]/options'
 
 type Props = {}
 
@@ -9,7 +9,7 @@ const Page = async (props: Props) => {
 	const session = await getServerSession(authOptions)
 	const res = await getUserData(session?.user?.id!, session?.accessToken)
 
-	console.log('res', res)
+	console.log("res", res)
 	return <Profile />
 }
 
