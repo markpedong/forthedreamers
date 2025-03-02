052e8244e360568c402bbeab93e709fa1582433a
markpedong <mark.pedong01@gmail.com>
2025-03-02 23:46:22 +0800

    added framer motion

--- app/(main)/components/index.tsx
+++ app/(main)/components/index.tsx
@@ -1,10 +1,16 @@
+'ues client'
+
 import Image from 'next/image'
-import React from 'react'
+import { FC } from 'react'
 import { FaArrowRight } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
-type Props = {}
+const Home: FC = () => {
+	const imageVariants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0 }
+	}
 
-const Home = (props: Props) => {
 	return (
 		<div className="max-w-7xl mx-auto h-screen px-12 py-6 relative">
 			<div className="flex justify-center items-center flex-col h-[75%] text-center gap-8 z-10 relative">
@@ -19,38 +25,66 @@ const Home = (props: Props) => {
 					<FaArrowRight size={12} />
 				</div>
 			</div>
-			<Image
-				src="/images/shirt1.png"
-				alt="shirt1"
-				width={240}
-				height={240}
-				className="absolute right-0 top-1/2 rounded-sm"
-				sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
-			/>
-			<Image
-				src="/images/shirt2.png"
-				alt="shirt2"
-				width={150}
-				height={150}
-				className="absolute left-0 top-[15%] rounded-sm"
-				sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
-			/>
-			<Image
-				src="/images/shirt3.png"
-				alt="shirt3"
-				width={150}
-				height={150}
-				className="md:absolute left-0 top-3/4 rounded-sm md:block hidden"
-				sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
-			/>
-			<Image
-				src="/images/shirt4.png"
-				alt="shirt4"
-				width={150}
-				height={150}
-				className="md:absolute right-[35%] top-[-10%] rounded-sm md:block hidden"
-				sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
-			/>
+			<motion.div
+				initial="hidden"
+				animate="visible"
+				variants={imageVariants}
+				transition={{ duration: 0.5, delay: 0.2 }}
+			>
+				<Image
+					src="/images/shirt1.png"
+					alt="shirt1"
+					width={240}
+					height={240}
+					className="absolute right-0 top-1/2 rounded-sm"
+					sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
+				/>
+			</motion.div>
+			<motion.div
+				initial="hidden"
+				animate="visible"
+				variants={imageVariants}
+				transition={{ duration: 0.5, delay: 0.4 }}
+			>
+				<Image
+					src="/images/shirt2.png"
+					alt="shirt2"
+					width={150}
+					height={150}
+					className="absolute left-0 top-[15%] rounded-sm"
+					sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
+				/>
+			</motion.div>
+			<motion.div
+				initial="hidden"
+				animate="visible"
+				variants={imageVariants}
+				transition={{ duration: 0.5, delay: 0.6 }}
+			>
+				<Image
+					src="/images/shirt3.png"
+					alt="shirt3"
+					width={150}
+					height={150}
+					className="md:absolute left-0 top-3/4 rounded-sm md:block hidden"
+					sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
+				/>
+			</motion.div>
+			<motion.div
+				initial="hidden"
+				animate="visible"
+				variants={imageVariants}
+				transition={{ duration: 0.5, delay: 0.8 }}
+			>
+				<Image
+					src="/images/shirt4.png"
+					alt="shirt4"
+					width={150}
+					height={150}
+					className="md:absolute right-[35%] top-[-10%] rounded-sm md:block hidden"
+					sizes="(max-width: 600px) 5vw, (max-width: 1024px) 33vw, 240px"
+				/>
+			</motion.div>
 		</div>
 	)
 }
