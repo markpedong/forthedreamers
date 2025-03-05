b55b90e6bba716749d7c99d4025a45197e3af2a4
markpedong <mark.pedong01@gmail.com>
2025-03-05 19:06:01 +0800

    buttpn

--- app/(main)/profile/components/payment-methods.tsx
+++ app/(main)/profile/components/payment-methods.tsx
@@ -1,18 +1,39 @@
 import PaymentMethods from '@/components/profile/payment-methods'
 import React, { FC } from 'react'
 import { PaymentMethods as TPaymentMethod } from '@prisma/client'
+import AddEditPaymentMethods from '@/components/profile/payment-methodsAddEdit'
+import { Typography } from 'antd'
+import { Button, useDisclosure } from '@heroui/react'
+import { useAppDispatch } from '@/redux/store'
+import { setPaymentMethod } from '@/redux/slices/userSlice'
 type Props = {
-  data: TPaymentMethod[]
+	data: TPaymentMethod[]
 }
 
 const PaymentMethod: FC<Props> = ({ data }) => {
-  return (
-    <div>
-      {data?.map(method => (
-        <PaymentMethods key={method.id} method={method} />
-      ))}
-    </div>
-  )
+	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+	const dispatch = useAppDispatch()
+	return (
+		<div>
+			<AddEditPaymentMethods isOpen={isOpen} onOpenChange={onOpenChange} />
+			<div className="flex justify-between">
+				<Typography.Title level={4}>Payment Methods</Typography.Title>
+				<Button
+					onPress={() => {
+						dispatch(setPaymentMethod(null))
+						onOpen()
+					}}
+					className="customButton1"
+					size="sm"
+				>
+					New
+				</Button>
+			</div>
+			{data?.map(method => (
+				<PaymentMethods key={method.id} method={method} openPaymentMethod={() => onOpen()} />
+			))}
+		</div>
+	)
 }
 
 export default PaymentMethod
