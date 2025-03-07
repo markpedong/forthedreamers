aeb1697ae4da5727724388fdcc5bc94242632bb2
atlas <atlas.wbridges@gmail.com>
2025-03-07 09:50:12 +0800

    seller image

--- app/(auth)/seller/index.tsx
+++ app/(auth)/seller/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 import { Button, Form, Input } from '@heroui/react'
 import { Typography } from 'antd'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -11,18 +12,30 @@ const Seller = (props: Props) => {
   return (
     <div className={styles.sellerWrapper}>
       <div className={styles.sellerContainer}>
-        <div className="flex flex-col items-center justify-center">
-          <Typography.Title level={3}>Seller Login</Typography.Title>
-          <Typography.Text>Access your dashboard to manage orders, products, and more.</Typography.Text>
+        <div className="w-full flex flex-col gap-10">
+          <div className="flex flex-col items-center justify-center">
+            <Typography.Title level={3}>Seller Login</Typography.Title>
+            <Typography.Text>Access your dashboard to manage orders, products, and more.</Typography.Text>
+          </div>
+          <Form className="w-full gap-4">
+            <Input placeholder="Email" name="email" type="email" isRequired />
+            <Input placeholder="Password" name="password" type="password" isRequired />
+            <Input placeholder="Confirm Password" name="confirmPassword" type="password" isRequired />
+            <Button radius="sm" color="default" type="submit" className="mt-5 customButton1" fullWidth>
+              Login
+            </Button>
+          </Form>
         </div>
-        <Form className="w-full gap-4">
-          <Input placeholder="Email" name="email" type="email" isRequired />
-          <Input placeholder="Password" name="password" type="password" isRequired />
-          <Input placeholder="Confirm Password" name="confirmPassword" type="password" isRequired />
-          <Button radius="sm" color="default" type="submit" className="mt-5 customButton1" fullWidth>
-            Login
-          </Button>
-        </Form>
+        <div className={styles.imgWrapper}>
+          <Image
+            src={`/images/login_cover-2.webp`}
+            alt=""
+            fill
+            sizes="60vw"
+            className="rounded-md"
+            priority
+          />
+        </div>
       </div>
     </div>
   )
