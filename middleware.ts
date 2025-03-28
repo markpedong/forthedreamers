b41e77383684ba0ba7d609f6fc822925f44d8227
markpedong <mark.pedong01@gmail.com>
2025-03-28 22:51:21 +0800

    adsdas

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import { AUTH_SECRET } from './constants'
 import { USER_ROLE } from '@prisma/client'
+import prisma from './db'
 
 export const middleware = async (request: NextRequest) => {
   const token = await getToken({ req: request, secret: AUTH_SECRET })
   const path = request.nextUrl.pathname
+  const referer = request.headers.get('referer') || '/'
 
   const protectedRoutes = ['/profile', '/checkout', '/cart']
   const sellerRestrictedRoutes = ['/profile'] // Sellers cannot access profile
@@ -21,6 +23,10 @@ export const middleware = async (request: NextRequest) => {
     if (userRole === USER_ROLE.USER && userRestrictedRoutes.includes(path)) {
       return NextResponse.redirect(new URL('/unauthorized', request.url))
     }
+
+    // if (path === '/checkout' && (await prisma.carts.findMany({ where: { userId: token.sub, deletedAt: null } })).length === 0) {
+    //   return NextResponse.redirect(new URL(referer, request.url))
+    // }
 
     if (path === '/login') {
       return NextResponse.redirect(new URL('/', request.url))
