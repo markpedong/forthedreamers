f2e36abd9cd903da1af7f1bf58b6219421cff494
markpedong <mark.pedong01@gmail.com>
2025-04-04 11:54:24 +0800

    Refactor: Move request utils from utils to lib/server directory

--- app/(auth)/checkout/page.tsx
+++ app/(auth)/checkout/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Checkout from './components'
 import { getServerSession } from 'next-auth'
 import authOptions from '@/app/api/auth/[...nextauth]/options'
-import { getAddress, getPaymentMethod } from '@/utils/request'
+import { getAddress, getPaymentMethod } from '@/lib/server'
 
 const Page = async () => {
 	const session = await getServerSession(authOptions)
