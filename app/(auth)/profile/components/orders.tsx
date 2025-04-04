a72609cf71f1ff667c1869031855610c63c1cb87
markpedong <mark.pedong01@gmail.com>
2025-04-04 11:31:01 +0800

    Refactor orders component to use React Query for data fetching and pagination

--- app/(auth)/profile/components/orders.tsx
+++ app/(auth)/profile/components/orders.tsx
@@ -5,40 +5,39 @@ import { TOrderItems } from '@/constants/types'
 import { getOrders } from '@/lib/server'
 import { Pagination } from '@heroui/react'
 import { Orders as TOrders } from '@prisma/client'
+import { useQuery } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
 import React, { FC, useEffect, useState } from 'react'
 
 type Props = {}
 
 const OrderList: FC<Props> = () => {
-  const ordersPerPage = 5
-  const [currentPage, setCurrentPage] = useState(1)
-  const [data, setData] = useState<TOrderItems[]>([])
-  const totalPages = Math.ceil(data.length / ordersPerPage)
-  const currentOrders = data.slice((currentPage - 1) * ordersPerPage, currentPage * ordersPerPage)
-  const { data: session } = useSession()
+	const ordersPerPage = 5
+	const [currentPage, setCurrentPage] = useState(1)
+	const { data: session } = useSession()
+	const { data } = useQuery<TOrderItems[]>({
+		queryKey: ['orders', session?.user?.id],
+		queryFn: async () => {
+			const response = await getOrders(`${session?.user?.id}`)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getOrders(`${session?.user?.id}`)
+			return response.data
+		}
+	})
+	const totalPages = Math.ceil(data!.length / ordersPerPage)
+	const currentOrders = data?.slice((currentPage - 1) * ordersPerPage, currentPage * ordersPerPage)
 
-      setData(response.data)
-    }
-    fetchData()
-  }, [])
-
-  return (
-    <div>
-      {currentOrders?.map(order => (
-        <Orders order={order} key={order.id} />
-      ))}
-      {totalPages > 1 && (
-        <div className="flex justify-center mt-6">
-          <Pagination total={totalPages} initialPage={1} page={currentPage} onChange={setCurrentPage} />
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div>
+			{currentOrders?.map(order => (
+				<Orders order={order} key={order.id} />
+			))}
+			{totalPages > 1 && (
+				<div className="flex justify-center mt-6">
+					<Pagination total={totalPages} initialPage={1} page={currentPage} onChange={setCurrentPage} />
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default OrderList
--- components/themes/index.tsx
+++ components/themes/index.tsx
@@ -3,21 +3,47 @@ import React, { FC } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { ConfigProvider, theme } from 'antd'
+import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const ThemesProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+	let browserQueryClient: QueryClient | undefined = undefined
 	const darkMode = useAppSelector(state => state.app.darkMode)
+
+	const makeQueryClient = () => {
+		return new QueryClient({
+			defaultOptions: {
+				queries: {
+					// With SSR, we usually want to set some default staleTime
+					// above 0 to avoid refetching immediately on the client
+					staleTime: 6 * 1000 // 6 sec
+				}
+			}
+		})
+	}
+
+	const getQueryClient = () => {
+		if (isServer) {
+			return makeQueryClient()
+		} else {
+			if (!browserQueryClient) browserQueryClient = makeQueryClient()
+			return browserQueryClient
+		}
+	}
+	const queryClient = getQueryClient()
 
 	return (
 		<NextThemesProvider attribute="class" defaultTheme={darkMode ? 'dark' : 'light'}>
 			<AntdRegistry>
-				<ConfigProvider
-					theme={{
-						algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
-						token: { fontFamily: 'Sora', colorPrimary: '#000' }
-					}}
-				>
-					{children}
-				</ConfigProvider>
+				<QueryClientProvider client={queryClient}>
+					<ConfigProvider
+						theme={{
+							algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+							token: { fontFamily: 'Sora', colorPrimary: '#000' }
+						}}
+					>
+						{children}
+					</ConfigProvider>
+				</QueryClientProvider>
 			</AntdRegistry>
 		</NextThemesProvider>
 	)
