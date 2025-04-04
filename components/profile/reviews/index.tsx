25e7528f20d4202151750d210e694703f38a2ad3
markpedong <mark.pedong01@gmail.com>
2025-04-04 14:27:01 +0800

    link.

--- components/profile/reviews/index.tsx
+++ components/profile/reviews/index.tsx
@@ -1,22 +1,25 @@
 import { TReviewItem } from '@/constants/types'
 import { dateFormatter } from '@/utils/helpers'
-import { Card, CardBody, Divider } from '@heroui/react'
-import { Icon } from '@iconify/react'
-import { Reviews as TReview } from '@prisma/client'
+import { Card, CardBody, Divider, Link } from '@heroui/react'
 import { Rate } from 'antd'
-import React, { FC } from 'react'
+import { useRouter } from 'next/navigation'
+import { FC } from 'react'
 
 type Props = {
 	data: TReviewItem
 }
 
 const Review: FC<Props> = ({ data }) => {
+	const { push } = useRouter()
+
 	return (
 		<Card key={data.id} className="w-full">
 			<CardBody>
 				<div className="flex flex-col gap-2">
 					<div className="flex items-center justify-between">
-						<p className="font-medium">{data.product.name}</p>
+						<Link className="font-medium" href={`/products/${data.productId}`} target="_blank">
+							{data.product.name}
+						</Link>
 						<p className="text-small text-default-500">{dateFormatter(data.createdAt)}</p>
 					</div>
 					<div className="flex items-center gap-2">
