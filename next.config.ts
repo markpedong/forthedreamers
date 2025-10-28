391bdc2b1607362c4ece9f44b691d217daecf09f
markpedong <mark.pedong01@gmail.com>
2025-10-28 18:49:53 +0800

    update next config.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  reactStrictMode: false,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  experimental: {
+    authInterrupts: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
