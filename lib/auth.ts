24d5d3dc48364a0c1b47ab5637f30778dcad7956
markpedong <mark.pedong01@gmail.com>
2025-10-28 18:41:25 +0800

    as

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,4 @@
 import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "./prisma";
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, { provider: "postgresql" }),
 });
