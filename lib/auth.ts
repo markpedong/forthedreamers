ff986b7b09a544e41dfa51a8b87ba31ba29806f3
markpedong <mark.pedong01@gmail.com>
2025-10-28 18:45:11 +0800

    feat: update environment variables and enhance authentication setup with Prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,16 @@
 import { betterAuth } from "better-auth";
+import prisma from "./prisma";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
 
 export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: "offline",
+      prompt: "select_account consent",
+    }
+  },
 });
