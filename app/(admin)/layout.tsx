bd1eefbf54a443e08d84070536f0348749e80c67
markpedong <mark.pedong01@gmail.com>
2025-11-02 01:04:39 +0800

    chore: sidebar fix.

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,7 +1,7 @@
 import { getSession, permissionListUsers } from '@/lib/server-actions';
 import { redirect } from 'next/navigation';
-import AdminSidebar from './components/sidebar';
-import AdminHeader from './components/header';
+import AdminSidebar from './components/admin-sidebar';
+import AdminHeader from './components/admin-header';
 import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default async function AdminLayout({
@@ -21,7 +21,7 @@ export default async function AdminLayout({
 
   return (
     <SidebarProvider>
-      <div className='flex h-screen bg-background'>
+      <div className='flex h-screen bg-background w-full'>
         <AdminSidebar />
 
         <div className='flex-1 flex flex-col overflow-hidden'>
