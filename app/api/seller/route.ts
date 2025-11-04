e3c27a3e98b59fec8771fc739920dbc0bed36c19
markpedong <mark.pedong01@gmail.com>
2025-11-05 00:33:49 +0800

    update.

--- app/api/seller/route.ts
+++ app/api/seller/route.ts
@@ -1,3 +1,4 @@
+import { USER_ROLE } from "@/generated/prisma";
 import prisma from "@/lib/prisma";
 import { errorResponse, successResponse } from "@/lib/server-helper";
 import { NextRequest } from "next/server";
@@ -12,6 +13,11 @@ export async function POST(req: NextRequest) {
     if (!userID || typeof userID !== "string") {
       return errorResponse("userID is required");
     }
+
+    await prisma.user.update({
+      where: { id: userID },
+      data: { role: USER_ROLE.SELLER },
+    });
 
     await prisma.seller.create({
       data: {
