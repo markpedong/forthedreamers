b52afab5ae24db3e837c3ac8bcfaa566e653dc1b
markpedong <mark.pedong01@gmail.com>
2025-11-14 01:48:42 +0800

    asdasdsadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,23 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import MainProvider from '@/components/provider/main-provider'
-import { geist } from './font'
+import localFont from 'next/font/local'
+
+const geist = localFont({
+  src: [
+    {path: '../public/font/geist/Geist-Thin.ttf', weight: '100', style: 'normal'},
+    {path: '../public/font/geist/Geist-ExtraLight.ttf', weight: '200', style: 'normal'},
+    {path: '../public/font/geist/Geist-Light.ttf', weight: '300', style: 'normal'},
+    {path: '../public/font/geist/Geist-Regular.ttf', weight: '400', style: 'normal'},
+    {path: '../public/font/geist/Geist-Medium.ttf', weight: '500', style: 'normal'},
+    {path: '../public/font/geist/Geist-SemiBold.ttf', weight: '600', style: 'normal'},
+    {path: '../public/font/geist/Geist-Bold.ttf', weight: '700', style: 'normal'},
+    {path: '../public/font/geist/Geist-ExtraBold.ttf', weight: '800', style: 'normal'},
+    {path: '../public/font/geist/Geist-Black.ttf', weight: '900', style: 'normal'}
+  ],
+  variable: '--font-geist',
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: {
--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,14 +1,19 @@
-import { getProduct } from '@/lib/http'
+import prisma from '@/lib/prisma'
 import ProductGallery from './components/product-gallery'
 import ProductOverview from './components/product-overview'
-import { OmittedProductFields, TProduct } from '@/lib/types'
 import VariantSelector from './components/variant-selector'
-import { API_ROUTE } from '@/constants/enum'
+import { OmittedProductFields, TVariant } from '@/lib/types'
 
 export async function generateStaticParams() {
-  const products = (await fetch(API_ROUTE.PRODUCTS).then(res => res.json())) as {data: TProduct[]}
+  const products = await prisma.product.findMany({
+    include: {
+      variants: true,
+      specs: true,
+      category: true
+    }
+  })
 
-  return products.data?.map(product => ({slug: product.slug})) ?? []
+  return products?.map(product => ({slug: product.slug})) ?? []
 }
 
 interface ProductPageProps {
@@ -17,15 +22,26 @@ interface ProductPageProps {
 
 const ProductPage = async ({params}: ProductPageProps) => {
   const {slug} = await params
-  const products = await getProduct(slug)
+  const products = await prisma.product.findUnique({
+    where: {slug},
+    include: {
+      specs: {
+        omit: {createdAt: true, updatedAt: true, productId: true}
+      },
+      category: {omit: {createdAt: true, updatedAt: true}},
+      variants: {
+        omit: {createdAt: true, updatedAt: true, productId: true}
+      }
+    }
+  })
 
   return (
     <main className='min-h-screen bg-background'>
       <div className='mx-auto max-w-6xl px-4 py-16 sm:px-6 lg:px-8 grid gap-16 lg:grid-cols-2'>
-        <ProductGallery images={products.data?.images ?? []} />
+        <ProductGallery images={products?.images ?? []} />
         <div className='flex flex-col'>
-          <ProductOverview product={products.data as OmittedProductFields} />
-          <VariantSelector variants={products.data?.variants ?? []} />
+          <ProductOverview product={products as OmittedProductFields} />
+          <VariantSelector variants={products?.variants as unknown as TVariant[]} />
           {/*  <AddToCartSection product={mockProduct} /> */}
         </div>
       </div>
